749edab76d35e0df950528863dafe8273b05e14f
Iliya Manolov <ilmanfordinner@gmail.com>
2016-05-29 17:00:38 +0300

    Created Project
    Created the project

---
+++ UI_Test_Generator/App.hpp
@@ -0,0 +1,25 @@
+#ifndef APP_HPP
+#define APP_HPP
+
+class App
+{
+
+
+    public:
+        App()
+        {
+
+        }
+
+        void run()
+        {
+
+        }
+};
+
+
+
+
+
+
+#endif // APP_HPP
